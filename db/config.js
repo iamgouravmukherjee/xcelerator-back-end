0fed70c3787c05b3ee2360c6a45e885f7cd49584
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-04-15 21:52:37 +0530

    added mlab db connectivity

--- db/config.js
+++ db/config.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // mongoose.connect("mongodb://justwork925:tomatobox91!@ds159767.mlab.com:59767/xcelerator", { useNewUrlParser: true });
-mongoose.connect("mongodb://localhost:27017/xcelerator", { useNewUrlParser: true });
+mongoose.connect("mongodb://justwork925:tomatobox91!@ds133256.mlab.com:33256/xcelerator", { useNewUrlParser: true });
 const db = mongoose.connection; 
 
 db.on('error', (err) => {
-   console.log('failed to connect to database');
+   console.log('failed to connect to database', err);
 });
 
 db.once('open', () => {
